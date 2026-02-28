4eb1bdcce79090e22b566e3ddea44cb9abdfe7ea
manufosela <manufosela@gmail.com>
2026-02-28 23:50:16 +0100

    chore: bump client version to 1.151.0

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - Do not edit manually
-export const version = '1.150.0';
-export const lastUpdated = '2026-02-28T22:00:16.042Z';
-export const lastBuildCommit = '5009a0f0cd66f30af217aa0f9ba01a05ef4cdc15';
+export const version = '1.151.0';
+export const lastUpdated = '2026-02-28T22:49:16.643Z';
+export const lastBuildCommit = '55fd210b735ee37bc78260a74aae4faf9473d691';
 export const instance = 'manufosela';
