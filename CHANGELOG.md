4fdf503266afa3e1810cbd6348e529563b94eade
manufosela <manufosela@gmail.com>
2026-03-04 19:36:41 +0100

    chore: bump client version to 1.162.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ Auto-generated from git commits on each build.
 
 ### Added
 
+- Split cleanview into 3 tabs (A Validar, Mis Tareas, Todas)
+
+## [1.161.0] - 2026-03-04
+
+### Added
+
 - Add cleanview link in adminproject header
 
 ## [1.160.0] - 2026-03-04
--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - Do not edit manually
-export const version = '1.161.0';
-export const lastUpdated = '2026-03-04T18:24:36.213Z';
-export const lastBuildCommit = 'f6366a58a9c6d16b81b20bc435343b5564da41c6';
+export const version = '1.162.0';
+export const lastUpdated = '2026-03-04T18:36:40.064Z';
+export const lastBuildCommit = '623bf8307ef2a80d9a40c318dd83b7276f52b7a4';
 export const instance = 'manufosela';
