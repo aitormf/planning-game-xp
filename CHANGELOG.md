21b279d6d42c9c0160d37a37f07b68a948e38ee7
Mánu Fosela <mfosela@geniova.com>
2026-03-05 16:40:22 +0100

    chore: bump client version to 1.164.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ Auto-generated from git commits on each build.
 
 ### Fixed
 
+- Stabilize app shell routing and nav
+
+## [1.164.2] - 2026-03-05
+
+### Fixed
+
 - Force full page reload on navigation to prevent blank pages
 
 ## [1.164.1] - 2026-03-05
--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - Do not edit manually
-export const version = '1.164.2';
-export const lastUpdated = '2026-03-05T05:06:23.780Z';
-export const lastBuildCommit = 'da9ce8ecb2f916250f82bcbf153dc0368928327c';
-export const instance = 'manufosela';
+export const version = '1.164.3';
+export const lastUpdated = '2026-03-05T15:38:41.422Z';
+export const lastBuildCommit = 'c48ce67b7599631647caa182eee1bc27f2852dfe';
+export const instance = 'geniova';
