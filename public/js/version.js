cafc94402c94d22b27e7f390d273a47af87a8228
Mánu Fosela <mfosela@geniova.com>
2026-03-05 16:46:55 +0100

    chore: sync lastBuildCommit

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - Do not edit manually
 export const version = '1.164.3';
 export const lastUpdated = '2026-03-05T15:38:41.422Z';
-export const lastBuildCommit = 'c48ce67b7599631647caa182eee1bc27f2852dfe';
+export const lastBuildCommit = '21b279d6d42c9c0160d37a37f07b68a948e38ee7';
 export const instance = 'geniova';
