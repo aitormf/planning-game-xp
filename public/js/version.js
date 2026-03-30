0726daf0969eb701be5489b6889a936f0e7cbb36
manufosela <manufosela@gmail.com>
2026-03-30 16:22:11 +0200

    fix: use query params instead of dynamic route for public view page

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - Do not edit manually
 export const version = '1.168.0';
 export const lastUpdated = '2026-03-30T14:20:12.480Z';
-export const lastBuildCommit = '00ebac5ce7bc48bfa3e40d584e664cdd580400dc';
+export const lastBuildCommit = '15e43cc0dbe3e22be7067c9292c81f2d8b8be7e3';
 export const instance = 'geniova';
