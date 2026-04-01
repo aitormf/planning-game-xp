71b1f57175136690b04ec19c9984a5cfaeda62bd
Mánu Fosela <mfosela@geniova.com>
2026-04-01 19:01:11 +0200

    chore: bump client version to 1.175.1

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - Do not edit manually
 export const version = '1.175.1';
-export const lastUpdated = '2026-04-01T16:54:36.730Z';
-export const lastBuildCommit = '771cb50031135d1bcba59ce41598f755a3dbcfe2';
+export const lastUpdated = '2026-04-01T17:01:07.265Z';
+export const lastBuildCommit = '116d8d10e74722ffc3c5b05ee203f0ca7ea7f52f';
 export const instance = 'geniova';
