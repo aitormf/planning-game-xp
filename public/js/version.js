0cd8db6ba5950cd4cdfb6eddee213e3bbe741976
aitormf <aitormf@users.noreply.github.com>
2026-04-08 09:25:49 +0200

    chore: bump client version to 1.183.4

--- public/js/version.js
+++ public/js/version.js
@@ -1,5 +1,5 @@
 // Auto-generated version file - Do not edit manually
 export const version = '1.183.4';
-export const lastUpdated = '2026-04-08T07:21:00.190Z';
-export const lastBuildCommit = 'eb6185eba240d6331a122a1e4132789274a83e46';
+export const lastUpdated = '2026-04-08T07:25:49.479Z';
+export const lastBuildCommit = 'a31722c354e4647f3c22d30aaf379af458f4748b';
 export const instance = 'planning-game-amf';
